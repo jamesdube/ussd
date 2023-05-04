113edc48b367b3c5e03fc86de972ef43ab22690c
James Dube <jamesdylandube@gmail.com>
2023-05-04 03:38:43 +0200

    fix count again

--- pkg/ussd/processor.go
+++ pkg/ussd/processor.go
@@ -204,7 +204,7 @@ func handlePagination(framework *Framework, c *menu.Context, ctx *fiber.Ctx, mes
 
 		var count int
 
-		if (len(c.Pages)) > 1 {
+		if (len(c.Pages)) > 1 && c.CurrentPage > 1 {
 			pct := len(c.Pages[c.CurrentPage-2])
 			count = pct
 		}
